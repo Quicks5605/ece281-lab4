472587be4c5b0ee2adc84c394777d55d5b46e584
Quicks5605 <100815742+Quicks5605@users.noreply.github.com>
2024-04-10 22:27:25 -0600

    TDM changes

--- src/hdl/TDM4.vhd
+++ src/hdl/TDM4.vhd
@@ -54,21 +54,19 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
 entity TDM4 is
-	generic ( constant k_WIDTH : natural  := 4); -- bits in input and output
+	generic ( constant k_WIDTH : natural  := 7); -- bits in input and output
     Port ( i_clk		: in  STD_LOGIC;
            i_reset		: in  STD_LOGIC; -- asynchronous
-           i_D3 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-		   i_D2 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
 		   i_D1 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
 		   i_D0 		: in  STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
 		   o_data		: out STD_LOGIC_VECTOR (k_WIDTH - 1 downto 0);
-		   o_sel		: out STD_LOGIC_VECTOR (3 downto 0)	-- selected data line (one-cold)
+		   o_sel		: out STD_LOGIC	-- one-bit
 	);
 end TDM4;
 
 architecture behavioral of TDM4 is
 
-	signal   f_sel		 : unsigned(1 downto 0)	:= "00"; -- 2 bit counter output to select MUX input
+	signal   f_sel		 : unsigned	:= "0"; -- 1 bit counter output to select MUX input
 	
 begin	
 	
@@ -80,7 +78,7 @@ begin
 	twoBitCounter_proc : process(i_clk, i_reset)
 	begin
 		if i_reset = '1' then
-			f_sel <= "00";
+			f_sel <= "0";
 		elsif rising_edge(i_clk) then
 			f_sel <= f_sel + 1;
 		end if;
@@ -91,15 +89,9 @@ begin
 	-- CONCURRENT STATEMENTS ----------------------------
 	
 	-- output MUXs
-	o_DATA <= i_D3 when f_sel = "11" else
-			  i_D2 when f_sel = "10" else
-			  i_D1 when f_sel = "01" else
-			  i_D0;
+	o_DATA <= i_D1 when f_sel = "1" else i_D0;
 			  
-	o_SEL  <=  "0111" when f_sel = "11" else
-			   "1011" when f_sel = "10" else
-			   "1101" when f_sel = "01" else
-			   "1110";
+	o_SEL  <=  '1' when f_sel = "1" else '0';
 		
 end behavioral;
 
